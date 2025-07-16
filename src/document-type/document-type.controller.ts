4dfbf6c09f7807a51df69d4d6c9321cf6bfbe9ed
Rafael Paiva <rafa_klose@hotmail.com>
2025-07-16 17:34:42 -0300

    fix: resolver tipagem

--- src/document-type/document-type.controller.ts
+++ src/document-type/document-type.controller.ts
@@ -1,12 +1,13 @@
 import { Controller, Post, Body } from '@nestjs/common';
 import { DocumentTypeService } from './document-type.service';
+import type { CreateDocumentTypeDto } from './dto/create-document-type.dto';
 
 @Controller('document-types')
 export class DocumentTypeController {
   constructor(private readonly service: DocumentTypeService) {}
 
   @Post()
-  create(@Body() data: any) {
+  create(@Body() data: CreateDocumentTypeDto) {
     return this.service.create(data);
   }
 }
--- src/document/document.controller.ts
+++ src/document/document.controller.ts
@@ -1,22 +1,25 @@
 import { Controller, Post, Body, Get, Query } from '@nestjs/common';
 import { DocumentService } from './document.service';
+import type { SendDocumentDto } from './dto/send-document.dto';
+import type { LinkDocumentDto } from './dto/link-document.dto';
+import type { UnlinkDocumentDto } from './dto/unlink-document.dto';
 
 @Controller('documents')
 export class DocumentController {
   constructor(private readonly service: DocumentService) {}
 
   @Post('send')
-  sendDocument(@Body() body: any) {
+  sendDocument(@Body() body: SendDocumentDto) {
     return this.service.sendDocument(body);
   }
 
   @Post('link')
-  linkDocuments(@Body() body: any) {
+  linkDocuments(@Body() body: LinkDocumentDto) {
     return this.service.linkDocuments(body);
   }
 
   @Post('unlink')
-  unlinkDocuments(@Body() body: any) {
+  unlinkDocuments(@Body() body: UnlinkDocumentDto) {
     return this.service.unlinkDocuments(body);
   }
 
